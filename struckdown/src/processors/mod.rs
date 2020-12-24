6e669c08b893ec842c12c14afc736abfe7501197
Armin Ronacher <armin.ronacher@active-4.com>
2020-12-24 10:15:07 +0100

    Reuse code with builtin processor

--- struckdown/src/processors/mod.rs
+++ struckdown/src/processors/mod.rs
@@ -54,50 +54,46 @@ pub trait Processor {
     ) -> Box<dyn Iterator<Item = AnnotatedEvent<'data>> + 'data>;
 }
 
-/// Utility struct for processor configurations.
-#[derive(Debug, Deserialize, Clone)]
-#[serde(tag = "processor", rename_all = "snake_case")]
-pub enum BuiltinProcessor {
-    AutoAnchors(Box<AutoAnchors>),
-    TableOfContents(Box<TableOfContents>),
-    #[cfg(feature = "external-processor")]
-    External(Box<External>),
-    #[cfg(feature = "syntect-processor")]
-    Syntect(Box<Syntect>),
-    #[cfg(feature = "html-sanitizer-processor")]
-    HtmlSanitizer(Box<HtmlSanitizer>),
+macro_rules! builtin_processors {
+    (
+        $($(#[$attr:meta])* type $name:ident;)*
+    ) => {
+        /// Utility struct for processor configurations.
+        #[derive(Debug, Deserialize, Clone)]
+        #[serde(tag = "processor", rename_all = "snake_case")]
+        pub enum BuiltinProcessor {
+            $($(#[$attr])* $name(Box<$name>),)*
+        }
+
+        impl Processor for BuiltinProcessor {
+            fn apply<'data>(
+                self: Box<Self>,
+                iter: Box<dyn Iterator<Item = AnnotatedEvent<'data>> + 'data>,
+            ) -> Box<dyn Iterator<Item = AnnotatedEvent<'data>> + 'data> {
+                match *self {
+                    $($(#[$attr])* Self::$name(options) => options.apply(iter),)*
+                }
+            }
+
+            fn apply_ref<'data, 'options: 'data>(
+                &'options self,
+                iter: Box<dyn Iterator<Item = AnnotatedEvent<'data>> + 'data>,
+            ) -> Box<dyn Iterator<Item = AnnotatedEvent<'data>> + 'data> {
+                match self {
+                    $($(#[$attr])* Self::$name(options) => options.apply_ref(iter),)*
+                }
+            }
+        }
+    };
 }
 
-impl Processor for BuiltinProcessor {
-    fn apply<'data>(
-        self: Box<Self>,
-        iter: Box<dyn Iterator<Item = AnnotatedEvent<'data>> + 'data>,
-    ) -> Box<dyn Iterator<Item = AnnotatedEvent<'data>> + 'data> {
-        match *self {
-            BuiltinProcessor::AutoAnchors(options) => options.apply(iter),
-            BuiltinProcessor::TableOfContents(options) => options.apply(iter),
-            #[cfg(feature = "external-processor")]
-            BuiltinProcessor::External(options) => options.apply(iter),
-            #[cfg(feature = "syntect-processor")]
-            BuiltinProcessor::Syntect(options) => options.apply(iter),
-            #[cfg(feature = "html-sanitizer-processor")]
-            BuiltinProcessor::HtmlSanitizer(options) => options.apply(iter),
-        }
-    }
-
-    fn apply_ref<'data, 'options: 'data>(
-        &'options self,
-        iter: Box<dyn Iterator<Item = AnnotatedEvent<'data>> + 'data>,
-    ) -> Box<dyn Iterator<Item = AnnotatedEvent<'data>> + 'data> {
-        match self {
-            BuiltinProcessor::AutoAnchors(options) => options.apply_ref(iter),
-            BuiltinProcessor::TableOfContents(options) => options.apply_ref(iter),
-            #[cfg(feature = "external-processor")]
-            BuiltinProcessor::External(options) => options.apply_ref(iter),
-            #[cfg(feature = "syntect-processor")]
-            BuiltinProcessor::Syntect(options) => options.apply_ref(iter),
-            #[cfg(feature = "html-sanitizer-processor")]
-            BuiltinProcessor::HtmlSanitizer(options) => options.apply_ref(iter),
-        }
-    }
+builtin_processors! {
+    type AutoAnchors;
+    type TableOfContents;
+    #[cfg(feature = "external-processor")]
+    type External;
+    #[cfg(feature = "syntect-processor")]
+    type Syntect;
+    #[cfg(feature = "html-sanitizer-processor")]
+    type HtmlSanitizer;
 }
